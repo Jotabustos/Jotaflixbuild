be04a52ae5266f8c21cfb302a8e47c46aa8891cb
Jose Julián Bustos Díaz <jota.bustos@thekeytalent.com>
2019-03-13 13:01:09 +0100

    First commit

---
+++ precache-manifest.ab9e19cdfed4ac39b89d0375a5624e60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1edbf18f4938d98fbfcc7d2e00d6172",
+    "url": "/static/media/jotaflix.c1edbf18.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5a862210c418efb50f1d",
+    "url": "/static/js/main.5a862210.chunk.js"
+  },
+  {
+    "revision": "0ea80e6552b91331a8e6",
+    "url": "/static/js/2.0ea80e65.chunk.js"
+  },
+  {
+    "revision": "5a862210c418efb50f1d",
+    "url": "/static/css/main.49ff4278.chunk.css"
+  },
+  {
+    "revision": "775017471296debe906d0218bfe5c605",
+    "url": "/index.html"
+  }
+];
